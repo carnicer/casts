e66c58cfda25812be02f71db7e674d8d8c777103
Marc Gonzalez i Carnicer <badukaire@gmail.com>
2020-01-30 06:30:52 +0100

    handle format exception

--- cast.cpp
+++ cast.cpp
@@ -3,6 +3,7 @@
 
 #include <cstdint>
 #include <string>
+#include <stdexcept>
 
 int displayDouble(double d)
 {
@@ -13,9 +14,13 @@ int displayDouble(double d)
     ret = printf("original double:%g\n", d);
     ret = printf("C-style uint32_t:     %u\n", uCc);
     ret = printf("C++-static uint32_t:  %u\n", uSc);
-    puts("====");
 
     return ret;
+}
+
+int displaySeparator()
+{
+    return puts("====");
 }
 
 int main(int argc, char* argv[])
@@ -24,17 +29,30 @@ int main(int argc, char* argv[])
     {
         for(int i = 1 ; i < argc ; ++i)
         {
+            double d;
             printf("%s\n", argv[i]);
-            double d = std::stod(std::string(argv[i]));
+            try{
+                d = std::stod(std::string(argv[i]));
+            }
+            catch(const std::invalid_argument& exception_IA)
+            {
+                printf("wrong double format\n");
+                displaySeparator();
+                continue;
+            }
             displayDouble(d);
-            puts("====");
+            displaySeparator();
         }
     }
     else
     {
         puts("no parameters provided, using default test values");
+
         displayDouble(2.5e-4);
+        displaySeparator();
+
         displayDouble(2.5e+4);
+        displaySeparator();
     }
 }
 
