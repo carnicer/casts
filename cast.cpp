9bfba840b9d2fdce90cec6d0edc09e610a8a7e11
Marc Gonzalez i Carnicer <badukaire@gmail.com>
2020-01-30 06:09:42 +0100

    method for testing

--- cast.cpp
+++ cast.cpp
@@ -2,16 +2,23 @@
 #include <stdio.h>
 #include <cstdint>
 
-int main()
+int displayDouble(double d)
 {
     int ret;
-    double dNeg = 3.25e-9, dPos = 4.888;
-    uint32_t uNegC = (uint32_t)dNeg, uPosC = (uint32_t)dPos;
-    uint32_t uNeg = static_cast<uint32_t>(dNeg), uPos = static_cast<uint32_t>(dPos);
+    uint32_t uCc = (uint32_t)d;
+    uint32_t uSc = static_cast<uint32_t>(d);
 
-    ret = printf("original double. dNeg:%g, dPos:%g\n", dNeg, dPos);
-    ret = printf("C-style uint32_t. uNegC:%u, uPosC:%u\n", uNegC, uPosC);
-    ret = printf("C++-static uint32_t. uNeg:%u, uPos:%u\n", uNeg, uPos);
+    ret = printf("original double:%g\n", d);
+    ret = printf("C-style uint32_t:     %u\n", uCc);
+    ret = printf("C++-static uint32_t:  %u\n", uSc);
+    puts("====");
 
     return ret;
 }
+
+int main()
+{
+    displayDouble(2.5e-4);
+    displayDouble(2.5e+4);
+}
+
